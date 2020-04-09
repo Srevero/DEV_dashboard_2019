aa71db340eca6b6f4ace21c71839b1f2ee55779d
florent <florent.melin@epitech.eu>
2020-04-09 16:49:24 +0200

    add link with DB mongoDB

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -3,10 +3,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mongoose = require('mongoose');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.connect('mongodb+srv://adminIRC:fnmM8ulX9Si9Zzj1@ircdatabase-vkyrn.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connexion MongoDB success !'))
+    .catch(() => console.log('Connexion MongoDB fail !'));
 
 app.use(logger('dev'));
 app.use(express.json());
